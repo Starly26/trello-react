feff2d4b5964517bb7e1d7bbaa19c913ec375ecd
Starly26 <86514705+Starly26@users.noreply.github.com>
2022-01-25 12:22:47 +0600

    add redux-persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cardReduser from "./card/cardSlice";
 import commentReduser from "./comment/commentSlice";
 import authorReduser from "./author/authorSlice";
 
+const rootReducer = combineReducers({
+  cards: cardReduser,
+  comment: commentReduser,
+  author: authorReduser,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    cards: cardReduser,
-    comment: commentReduser,
-    author: authorReduser,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export const persistor = persistStore(store);
 export default store;
